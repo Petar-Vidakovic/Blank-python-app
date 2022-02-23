691c79853b4a0238f102933df7e70e184ebd3bc2
Petar-Vidakovic <Petarvidakovic94@gmail.com>
2022-02-23 16:48:07 +1000

    Added base for server and client

--- App.py
+++ App.py
@@ -7,16 +7,13 @@ vec = pygame.math.Vector2
 
 class App:
     def __init__(self):
-        self.screen = pygame.display.set_mode((500, 600))
+        self.screen = pygame.display.set_mode((600, 600))
         self.clock = pygame.time.Clock()
-        self.title = pygame.
         self.running = True
         self.state = 'start'
 
-
     def run(self):
         pygame.display.set_caption("Petar's App")
-        self.screen.fill(WHITE)
         while self.running:
             if self.state == 'start':
                 self.state_event()
@@ -35,5 +32,6 @@ class App:
         pass
 
     def state_draw(self):
+        self.screen.
         self.screen.fill(WHITE)
         pygame.display.update()
