d5207aa5cd230bf2485b580a93214c5840bce8ec
Petar-Vidakovic <Petarvidakovic94@gmail.com>
2022-02-27 19:16:02 +1000

    fix app page

--- App.py
+++ App.py
@@ -5,7 +5,6 @@ from Settings import *
 pygame.init()
 vec = pygame.math.Vector2
 
-
 class App:
     def __init__(self):
         self.startScreen = pygame.display.set_mode((600, 600))
@@ -19,7 +18,6 @@ class App:
             if self.state == 'start':
                 self.state_event()
                 self.state_draw()
-                self.button()
             self.clock.tick(120)
         pygame.quit()
         sys.exit()
@@ -32,18 +30,3 @@ class App:
     def state_draw(self):
         self.startScreen.fill(WHITE)
         pygame.display.update()
-
-    def button(self, msg, x, y, w, h, hovercolor, defaultcolor, action=None):
-        mouse = pygame.mouse.get_pos()
-        click = pygame.mouse.get_pressed()
-
-        if x + w > mouse[0] > x and y + h > mouse[1] > y:
-            pygame.draw.rect(startScreen, hovercolor, (x, y, w, h))
-            if click[0] == 1 and action != None:
-                action()
-        else:
-            pygame.draw.rect(startScreen, defaultcolor, (x, y, w, h))
-
-        textSurf, textRect = text_object(msg, font)
-        textRect.center = ((x + (w // 2)), (y + (h // 2)))
-        startScreen.blit(textSurf, textRect)
